daaa972ee93b5931d05fb50d70172089fb1f04a1
Huy-DNA <huydo862003@gmail.com>
2026-04-29 20:29:24 +0700

    chore: init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Typedown
+
+![Status](https://img.shields.io/badge/status-active-brightblue)
+![License](https://img.shields.io/badge/license-GPL-green)
+<a href="https://github.com/huydo862003/Fck-AI-Slop#plan"><img src="https://img.shields.io/badge/Human%20slop-90EE90"></a>
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            mdbook
+            mdbook-mermaid
+            rustc
+            cargo
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+        };
+      }
+    );
+}
