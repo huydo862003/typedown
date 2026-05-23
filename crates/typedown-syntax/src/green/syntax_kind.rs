9d0bb3dfb44b0cf0769740ffa477a286a531c731
Huy-DNA <huydo862003@gmail.com>
2026-05-23 10:45:01 +0700

    fix: token syntax kinds

--- crates/typedown-syntax/src/green/syntax_kind.rs
+++ crates/typedown-syntax/src/green/syntax_kind.rs
@@ -1,34 +1,23 @@
-//! See rowan's SyntaxKind: https://github.com/rust-analyzer/rowan/commits/master/src/green.rs
-//! - rowan is designed to be more generic so it just wraps around a u16
-//! - We're not aiming for genericity so we just define it to be an enum with an underlying u16
-
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 #[repr(u16)]
 pub enum SyntaxKind {
   // Top-level
   SourceFile = 0, // root: frontmatter + body
 
-  // Frontmatter
+  // Frontmatter (YAML mode) nodes
   Frontmatter = 100,
-
   Mapping,
   MappingEntry, // one key: value pair
   Key,
   Value,
-
   Sequence,
   SequenceItem, // one - item
 
-  // Body (TDR Markdown)
+  // Body (Markdown mode) nodes
   Body = 200,
   Heading,
   Paragraph,
-  PlainText, // Optimized for long proses for memort efficiency
   Blockquote,
-  CodeBlock,
-  InlineCode,
-  MathBlock,
-  InlineMath,
   Table,
   TableRow,
   TableCell,
@@ -39,33 +28,39 @@ pub enum SyntaxKind {
   ToggleList,
   ToggleListItem,
   CalloutBlock,      // ::: label ... :::
-  FootnoteBlock,     // ::: footnote
-  BibliographyBlock, // ::: bibtex
+  FootnoteBlock,     // ::: footnote ... :::
+  BibliographyBlock, // ::: bibtex ... :::
   Link,              // [text](url)
   Media,             // ![alt](src)
   FootnoteRef,       // [^key]
   Citation,          // [@key]
   Text,              // plain text run
 
-  // Expressions
+  // Expression nodes
   Expr = 300,
   InterpExpr, // ${ ... }
   TaggedExpr, // !tag value
   Tag,        // !string, !number, ...
 
-  // Tokens
+  // Tokens (leaf nodes emitted by the lexer)
   Ident = 400,
   Number,
-  DqStr, // "..."
-  SqStr, // '...'
-  BtStr, // `...`
+  DqStr,      // "..."
+  SqStr,      // '...'
+  InlineCode, // matched ` delimiters, content on same line
+  CodeBlock,  // matched ` delimiters, optional language tag, content between newlines
+  InlineMath, // matched $ delimiters, content on same line
+  MathBlock,  // matched $ delimiters, content between newlines
+  YamlOp,     // operators: +, -, ., ->, ==, etc.
 
-  // Punctuation / markers
+  // Punctuation and delimiters
   TripleDash = 500, // ---
   TripleColon,      // :::
   Colon,            // :
   Bang,             // !
   Dollar,           // $
+  LParen,           // (
+  RParen,           // )
   LBracket,         // [
   RBracket,         // ]
   LBrace,           // {
@@ -77,17 +72,16 @@ pub enum SyntaxKind {
   Caret,            // ^
   Star,             // *
   Tilde,            // ~
-  Backtick,         // `
   InterpStart,      // ${
-  InterpEnd,        // $}
+  InterpEnd,        // } closing an interpolation
 
   // Trivia
   Whitespace = 600,
   Newline,
-  Comment,
+  Comment, // # ...
   Indent,
   Dedent,
-  Eof, // End of file
+  Eof,
 
   // Error
   Error,
