a0ab18cd0f276c97ae51ade84df70e43a5903f82
Huy-DNA <huydo862003@gmail.com>
2026-06-02 01:54:54 +0700

    chore: init typedown-db crate

--- crates/typedown-db/src/lib.rs
+++ crates/typedown-db/src/lib.rs
@@ -1,1 +1,13 @@
+//! A salsa database for incremental Typedown compilation
+//! Salsa seems to remove the ParallelDatabase trait: https://github.com/salsa-rs/salsa/pull/1013
+//! I've scoured the salsa repo but it seems we have to do it manually now
+pub mod inputs;
+pub mod tracked;
 
+#[salsa::db]
+pub struct TypedownDatabase {
+  storage: salsa::Storage<Self>,
+}
+
+#[salsa::db]
+impl salsa::Database for TypedownDatabase {}
