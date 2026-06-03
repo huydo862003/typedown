85e232805ba8566ac53d2dc871d6e96a99a3a51e
Huy-DNA <huydo862003@gmail.com>
2026-06-03 19:35:49 +0700

    feat: basic Derived trait and DerivedId struct

--- crates/typedown-db/src/engine/derived.rs
+++ crates/typedown-db/src/engine/derived.rs
@@ -1,1 +1,11 @@
 //! Derived query engine for the incremental database
+
+use std::marker::PhantomData;
+
+/// A trait for a derived state
+/// Required to be Clone, Send and Sync
+pub trait Derived: Clone + Send + Sync {}
+
+/// A fast id for a derived state
+/// Input id is bound to a database's lifetime
+pub struct DerivedId<'db, T: Derived>(usize, PhantomData<T>, PhantomData<&'db ()>);
