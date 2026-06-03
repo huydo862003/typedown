442f2fbbfe433bc42eee580beea27faa36bc80bb
Huy-DNA <huydo862003@gmail.com>
2026-06-03 19:32:43 +0700

    feat: basic Input trait and InputId struct

--- crates/typedown-db/src/engine/input.rs
+++ crates/typedown-db/src/engine/input.rs
@@ -1,1 +1,11 @@
 //! Input declaration engine for the incremental database
+
+use std::marker::PhantomData;
+
+/// A trait for an input state
+/// Required to be Clone, Send and Sync
+pub trait Input: Clone + Send + Sync {}
+
+/// A fast id for an input state
+/// Input id is bound to a database's lifetime
+pub struct InputId<'db, T: Input>(usize, PhantomData<T>, PhantomData<&'db ()>);
