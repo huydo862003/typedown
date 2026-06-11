cf3a8d051ec4cd754f7e06092598e1be7d67e1cf
Huy-DNA <huydo862003@gmail.com>
2026-06-11 22:41:44 +0700

    feat: add symbol kind

--- crates/typedown-db/src/types/symbol/mod.rs
+++ crates/typedown-db/src/types/symbol/mod.rs
@@ -2,10 +2,16 @@ use typedown_macros::query_derived;
 
 use crate::types::GreenNode;
 
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum SymbolKind {
+  Schema,
+}
+
 #[query_derived]
 pub struct Symbol {
   #[id]
   node: GreenNode,
+  kind: SymbolKind,
 }
 
 #[query_derived]
