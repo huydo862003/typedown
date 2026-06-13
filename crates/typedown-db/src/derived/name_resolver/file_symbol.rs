e2c54f0f98d71f3ed68339bc0fda0f1d40d7d770
Huy-DNA <huydo862003@gmail.com>
2026-06-13 19:04:16 +0700

    fix: make members accept scope instead & rename symbol to file symbol

---
+++ crates/typedown-db/src/derived/name_resolver/file_symbol.rs
@@ -0,0 +1,23 @@
+use typedown_macros::query_derived;
+
+use crate::derived::get_vault_config::get_vault_config;
+use crate::types::{File, Project, Symbol, SymbolKind};
+use crate::{QueryDatabase, TypedownDatabase};
+
+#[query_derived]
+pub fn file_symbol(db: &TypedownDatabase, project: Project, file: File) -> Option<Symbol> {
+  let config = get_vault_config(db, project);
+  let schema_dir = config.schema_dir(db);
+  let handles = project.handles(db);
+  let file_handle = file.handle(db);
+
+  let is_schema_file = handles
+    .iter()
+    .any(|(path, handle)| *handle == file_handle && path.starts_with(&schema_dir));
+
+  if is_schema_file {
+    return Some(Symbol::new(db, SymbolKind::Schema(file)));
+  }
+
+  None
+}
--- crates/typedown-db/src/derived/name_resolver/referee.rs
+++ crates/typedown-db/src/derived/name_resolver/referee.rs
@@ -1,6 +1,6 @@
 use typedown_macros::query_derived;
 
-use crate::types::{File, TdrNode, Project, Symbol};
+use crate::types::{File, Project, Symbol, TdrNode};
 use crate::{QueryDatabase, TypedownDatabase};
 
 #[query_derived]
