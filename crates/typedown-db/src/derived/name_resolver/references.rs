e80dd2bf72bd8e5f19c339ef794a935555e3c3a7
Huy-DNA <huydo862003@gmail.com>
2026-06-14 15:26:41 +0700

    fix: temporarily remove references query

--- crates/typedown-db/src/derived/name_resolver/references.rs
+++
@@ -1,9 +0,0 @@
-use typedown_macros::query_derived;
-
-use crate::types::{Project, References, Symbol};
-use crate::{QueryDatabase, TypedownDatabase};
-
-#[query_derived]
-pub fn references(db: &TypedownDatabase, project: Project, symbol: Symbol) -> References {
-  todo!()
-}
