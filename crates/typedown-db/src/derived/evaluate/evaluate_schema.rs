646d738efa05d062729fa94d47125be6253fe14a
Huy-DNA <huydo862003@gmail.com>
2026-06-14 18:06:20 +0700

    feat: implement evaluate_schema for builtin schema

--- crates/typedown-db/src/derived/evaluate/evaluate_schema.rs
+++ crates/typedown-db/src/derived/evaluate/evaluate_schema.rs
@@ -2,10 +2,18 @@
 
 use typedown_macros::query_derived;
 
-use crate::types::{Symbol, TypeResult};
+use crate::derived::get_builtin_types::get_schema_type;
+use crate::types::{BuiltinSchemaKind, Symbol, SymbolKind, TypeResult};
 use crate::{QueryDatabase, TypedownDatabase};
 
 #[query_derived]
 pub fn evaluate_schema(db: &TypedownDatabase, symbol: Symbol) -> TypeResult {
-  todo!();
+  match symbol.kind(db) {
+    SymbolKind::BuiltinSchema(BuiltinSchemaKind::Schema) => {
+      TypeResult::new(db, Box::new(get_schema_type(db)), vec![])
+    }
+    SymbolKind::UserDefinedSchema(_file) => {
+      todo!()
+    }
+  }
 }
