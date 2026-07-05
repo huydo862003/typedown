eb43d2df0f20ffb6dcb367ad622f8adf6e1f29f7
Huy-DNA <huydo862003@gmail.com>
2026-07-05 22:37:43 +0700

    feat: basic typedown serde context

--- crates/typedown-lang/src/serde.rs
+++ crates/typedown-lang/src/serde.rs
@@ -1,15 +1,29 @@
-use typedown_incremental::{DeserializeContext, SerializeContext};
+use typedown_incremental::{Decoder, DeserializeContext, Encoder, SerializeContext};
+
+use crate::{TypedownDecoder, TypedownEncoder};
 
 /// Serialize context for typedown
-pub struct TypedownSerializeContext {}
+pub struct TypedownSerializeContext<'a> {
+  encoder: TypedownEncoder<'a>,
+}
 
-impl TypedownSerializeContext {}
+impl<'a> TypedownSerializeContext<'a> {}
 
-impl SerializeContext for TypedownSerializeContext {}
+impl<'a> SerializeContext for TypedownSerializeContext<'a> {
+  fn encoder(&mut self) -> &mut dyn Encoder {
+    &mut self.encoder
+  }
+}
 
 /// Deserialize context for typedown
-pub struct TypedownDeserializeContext {}
+pub struct TypedownDeserializeContext<'a> {
+  decoder: TypedownDecoder<'a>,
+}
 
-impl TypedownDeserializeContext {}
+impl<'a> TypedownDeserializeContext<'a> {}
 
-impl DeserializeContext for TypedownDeserializeContext {}
+impl<'a> DeserializeContext for TypedownDeserializeContext<'a> {
+  fn decoder(&mut self) -> &mut dyn Decoder {
+    &mut self.decoder
+  }
+}
