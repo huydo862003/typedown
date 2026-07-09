37cdcd84d5acf2b9641963980bbf05629ba1cbaf
Huy-DNA <huydo862003@gmail.com>
2026-07-10 00:13:03 +0700

    chore: enable nightly rust toolchain

--- flake.nix
+++ flake.nix
@@ -2,25 +2,31 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    { nixpkgs, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+        rust-nightly = pkgs.rust-bin.nightly.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             mdbook
             mdbook-mermaid
-            rustc
-            cargo
-            rustfmt
-            clippy
-            rust-analyzer
+            rust-nightly
             nodejs
             pnpm
           ];
