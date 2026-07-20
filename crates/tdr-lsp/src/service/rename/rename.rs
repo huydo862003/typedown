b4fa35f9fcdda16d1640eb159aed41aa4706d5d1
Huy-DNA <huydo862003@gmail.com>
2026-07-20 17:41:15 +0700

    feat: partial implementation of rename

--- crates/tdr-lsp/src/service/rename/rename.rs
+++ crates/tdr-lsp/src/service/rename/rename.rs
@@ -1,7 +1,52 @@
-use lsp_types::{RenameParams, WorkspaceEdit};
+use lsp_types::{DocumentChangeOperation, RenameParams, WorkspaceEdit};
+use tdr_lang::{
+  db::derived::{
+    hir::lower_node,
+    name_resolver::{referee::referee, resolution_index::references},
+  },
+  syntax::ast::AstNode,
+};
 
-use crate::analysis::Analysis;
+use crate::{
+  analysis::Analysis,
+  service::rename::{types::RenameSymbol, utils::find_rename_symbol},
+  utils::{position::lsp_position_to_text_offset, uri::uri_to_path},
+};
 
-pub fn rename(_analysis: &Analysis, _params: RenameParams) -> Option<WorkspaceEdit> {
+pub fn rename(analysis: &Analysis, params: RenameParams) -> Option<WorkspaceEdit> {
+  let project = analysis.project;
+
+  // Locate the file of the rename request
+  let path = uri_to_path(&params.text_document_position.text_document.uri)?;
+  let file = *project.files(&analysis.db).get(&path)?;
+  let rope = analysis.file_rope(&path)?;
+
+  // Locate the offset of the rename request
+  let editor_pos = params.text_document_position.position;
+  let offset = lsp_position_to_text_offset(&rope, editor_pos)?;
+
+  // Find the symbol that is requested a rename + qualifying information
+  let rename_symbol = find_rename_symbol(&analysis.db, project, file, offset)?;
+
+  let original_symbol = match rename_symbol {
+    RenameSymbol::Fref { call_node } => referee(
+      &analysis.db,
+      lower_node(&analysis.db, project, file, call_node.syntax().clone()),
+    ),
+    RenameSymbol::Identifier { ident_node } => referee(
+      &analysis.db,
+      lower_node(&analysis.db, project, file, ident_node.syntax().clone()),
+    ),
+  }
+  .value(&analysis.db)?;
+
+  let references = references(&analysis.db, project, original_symbol).references(&analysis.db);
+
+  if references.len() == 0 {
+    return None;
+  }
+
+  let changes: Vec<DocumentChangeOperation> = vec![];
+
   todo!()
 }
